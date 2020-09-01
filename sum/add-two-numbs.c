4994e34caf9e8f7341acfeaa05ab32b519c72400
sj-log <sj-log@gmail.com>
2020-09-01 10:24:16 +0900

    add first exercise

---
+++ sum/add-two-numbs.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  add-two-numbs.c
+ *
+ *    Description: 
+ *
+ *        Version:  1.0
+ *        Created:  2020년 09월 01일 09시 14분 31초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+
+int main(){
+
+        int f;
+        int s;
+
+        printf("input first number : ");
+        scanf("%d", &f);
+
+        printf("input second number : ");
+        scanf("%d", &s);
+
+        printf("sum : %d ", f+s);
+
+        return 0;
+
+
+}
