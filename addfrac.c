3d7cc219151e049919af16af839c32a021a50d88
sj-log <sj-log@gmail.com>
2020-10-08 19:43:13 +0900

    update

--- addfrac.c
+++ addfrac.c
@@ -20,14 +20,14 @@
 
 int main(){
 
-        int num1,denom1,num2,denom2,result_num, result_denom; 
-        printf("Enter first fraction: "); 
-        scanf("%d / %d", &num1, &denom1); 
-        printf("Enter second fraction: "); 
-        scanf("%d / %d", &num2, &denom2); 
-        result_num = num1 * denom2 + num2 * denom1; 
-        result_denom = denom1 * denom2;
-        printf("The sum is %d/%d\n", result_num, result_denom);
+        int num1,denum1,num2,denum2,result_num, result_denum; 
+        printf("Enter two fractions seperated by a plus sign: "); 
+        scanf("%d/%d+%d/%d", &num1, &denum1, &num2, &denum2); 
+        
+        result_denum = denum1*denum2;
+        result_num = denum2*num1 + denum1*num2;
+
+        printf("The sum is %d/%d\n", result_num, result_denum);
 
         return 0;
 }
