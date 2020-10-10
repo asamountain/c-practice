c0466c03ce3cf57f263932f902e0a8af2b4500d4
sj-log <sj-log@gmail.com>
2020-10-10 18:00:45 +0900

    post update

--- section_5_1_3.c
+++ section_5_1_3.c
@@ -21,7 +21,29 @@ int main(){
         i=3;j=4;k=5;
         printf("%d ", i<j || ++j <k);
         printf("%d %d %d\n", i, j, k);
-        printf("1, 3 5 5");
+        printf("1, 3 4 5\n");
+        /*  because, right operand from || will not accessed. because i<j (left operand) is false. */
+
+
+        int a,b,c;
+        a=7; b=8; c=9;
+        printf("%d", a-7 && b++ < c);
+        printf("%d %d %d\n", a, b, c);
+        printf("0, 7, 8, 9");
+
+
+        int e,f,g;
+        e=7;f=8;g=9;
+        printf("%d ", (e=f)||(f=g));
+        printf("%d %d %d\n", e,f,g );
+        printf("0 because, false || false\n");
+
+
+        int l,m,n;
+        l=1;m=1;n=1;
+        printf("%d ", ++l || ++m && ++n);
+        printf("%d %d %d\n", l, m, n);
+        printf("1 , 2 2 2");
 
         return 0;
 }
